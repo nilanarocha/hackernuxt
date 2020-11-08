2c60409d2c08482426acd656fecc8662a9347da2
Nilana Rocha <nilanaarquitetura@gmail.com>
2020-11-09 00:27:09 +1100

    adding axios to your project

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Hello World</div>
+  <div class="f1 code">
+    Hello World
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.login }}</li>
+    </ul>
+  </div>
 </template>
+<script>
+import axios from "~/plugins/axios";
+export default {
+  asyncData() {
+    return axios.get("users").then(res => ({ users: res.data }));
+  }
+};
+</script>
